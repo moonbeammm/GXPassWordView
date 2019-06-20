2c84dcb442897e751106b7c3aadbf27b7a39956d
sunguangxin <sunguangxin@bilibili.com>
2019-06-20 19:48:57 +0800

    --user=sunguangxin --task=commit 2

--- GXPassWordView/AppDelegate.m
+++ GXPassWordView/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     NSLog(@"1");
+    NSLog(@"2");
     return YES;
 }
 
